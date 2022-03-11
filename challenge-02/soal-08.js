2e3933fb795a39892484e5fb83cbc25b2353b5d4
didapatria <didapatria3@gmail.com>
2022-03-11 19:29:30 +0700

    FINAL

--- challenge-02/soal-08.js
+++ challenge-02/soal-08.js
@@ -49,23 +49,53 @@ function getInfoPenjualan(dataPenjualan) {
           penulisTerlaris
 
       let bestseller = dataPenjualan[0].totalTerjual
+      let namePenulis
       for (let i = 1; i < dataPenjualan.length; i++) {
         if (dataPenjualan[i].totalTerjual > bestseller) {
           bestseller = dataPenjualan[i].totalTerjual
+          namePenulis = dataPenjualan[i].penulis
+        } namePenulis = dataPenjualan[0].penulis
+      }
+
+      let uniquePenulis = [];
+      for (let i=0; i<dataPenjualan.length; i++) {
+        if (uniquePenulis.indexOf(dataPenjualan[i].penulis) === -1 && dataPenjualan[i].penulis !== '') {
+          uniquePenulis.push(dataPenjualan[i].penulis);
         }
       }
 
-      let arrBaru = []
-      for (let i = 0; i < dataPenjualan.length; i++) {
-        if (dataPenjualan[i].penulis == 'Tere Liye') {
-          penulis = dataPenjualan[i].penulis
-          arrBaru.push(dataPenjualan[i].totalTerjual)
+      let dataPenulis = [],
+          countTerjual = 0,
+          countBuku = 0
+      for (let i=0; i<uniquePenulis.length; i++) {
+        for (let j=0; j<dataPenjualan.length; j++) {
+          if (dataPenjualan[j].penulis == uniquePenulis[i]) {
+            countTerjual = countTerjual + dataPenjualan[j].totalTerjual
+            countBuku++
+          }
         }
+        let obj = {};
+        obj['penulis'] = uniquePenulis[i];
+        obj['totalTerjual'] = countTerjual;
+        obj['totalBuku'] = countBuku;
+        dataPenulis.push(obj);
+        countTerjual = 0
+        countBuku = 0
       }
-      
-      let bestsellerPenulis = 0
-      for (let i = 0; i < arrBaru.length; i++) {
-        bestsellerPenulis += arrBaru[i]
+
+      let bestsellerPenulis = dataPenulis[0].totalTerjual
+      let nameBestPenulis
+      for (let i = 1; i < dataPenulis.length; i++) {
+        if (dataPenulis[i].totalTerjual > bestsellerPenulis) {
+          bestsellerPenulis = dataPenulis[i].totalTerjual
+          nameBestPenulis = dataPenulis[i].penulis
+        } nameBestPenulis = dataPenulis[0].penulis
+      }
+
+      if (bestsellerPenulis > bestseller) {
+        penulisTerlaris = nameBestPenulis
+      } else {
+        penulisTerlaris = namePenulis
       }
 
       for (let i = 0; i < dataPenjualan.length; i++) {
@@ -73,10 +103,6 @@ function getInfoPenjualan(dataPenjualan) {
         totalModal += dataPenjualan[i].hargaBeli * (dataPenjualan[i].totalTerjual + dataPenjualan[i].sisaStok)
         if (dataPenjualan[i].totalTerjual == bestseller) {
           produkBukuTerlaris = dataPenjualan[i].namaProduk
-          penulisTerlaris = dataPenjualan[i].penulis
-        }
-        if (bestsellerPenulis > bestseller) {
-          penulisTerlaris = penulis
         }
       }
 
