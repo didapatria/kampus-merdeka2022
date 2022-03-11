7f60018063cb7ace6ecf6f91948fe6bafdc1b1e0
didapatria <didapatria3@gmail.com>
2022-03-11 17:19:48 +0700

    4th Update soal-06.js

--- challenge-02/soal-06.js
+++ challenge-02/soal-06.js
@@ -2,7 +2,20 @@ function getAngkaTerbesarKedua(num) {
   // tulis kode logic kamu didalam blok ini ya
   if (num) {
     if (Array.isArray(num)) {
-      num.sort((x, y) => {return y - x})
+      // num.sort((x, y) => {return y - x})
+
+      // Bubble Sort
+      for (let i = 0; i < num.length; i++) {
+        for (let j = 0; j < num.length; j++) {
+          if (num[j] < num[j + 1]) {
+            let temp = num[j + 1];
+            num[j + 1] = num[j];
+            num[j] = temp;
+          }
+        }
+      }
+
+      // Unique / Filter
       let result = [num[0]]
       for (let i = 1; i < num.length; i++) {
         if (num[i-1] !== num[i]) {
