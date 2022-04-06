6328707b27747095cef5059047676e65d93fb843
didapatria <didapatria3@gmail.com>
2022-04-06 16:31:56 +0700

    only the search view but the search function doesn't work yet

--- challenge-04/src/pages/Home.jsx
+++ challenge-04/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react'
 import Navbar from '../components/Navbar'
+import Search from '../components/Search'
 
 export default function Home() {
   return (
     <Fragment>
       <Navbar />
+      <div className='-mt-12'>
+        <Search />
+      </div>
     </Fragment>
   )
 }
