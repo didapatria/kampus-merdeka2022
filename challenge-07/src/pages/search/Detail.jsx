981fec3516147d5d411b0e0fe3979ab79000a0ce
didapatria <didapatria3@gmail.com>
2022-05-26 06:21:12 +0700

    private-routes

--- challenge-07/src/pages/search/Detail.jsx
+++ challenge-07/src/pages/search/Detail.jsx
@@ -1,21 +1,32 @@
 import React, { Fragment, useEffect } from 'react'
+
+import { useDispatch, useSelector } from 'react-redux'
+
 import Navbar from '../../components/Navbar'
 import Search from '../../components/Search'
 import Details from '../../components/Details'
 import Footer from '../../components/Footer'
+
 import { useParams } from 'react-router'
-import { useDispatch, useSelector } from 'react-redux'
 import { fetchCar } from '../../actions/car'
+
+import { Navigate } from 'react-router-dom'
 
 export default function Detail() {
   const {id}  = useParams()
 
   const dispatch = useDispatch()
   const detailCar = useSelector((state) => state.carReducer)
+  const { isLoggedIn } = useSelector(state => state.auth)
 
   useEffect(() => {
     dispatch(fetchCar(id))
   }, [])
+
+
+  if (!isLoggedIn) {
+    return <Navigate to='/unauthorize' />
+  }
 
   return (
     <Fragment>
--- challenge-07/src/pages/search/Result.jsx
+++ challenge-07/src/pages/search/Result.jsx
@@ -1,14 +1,20 @@
 import React, { Fragment, useEffect } from 'react'
+
+import { useDispatch, useSelector } from 'react-redux'
+
 import Navbar from '../../components/Navbar'
 import Search from '../../components/Search'
 import Card from '../../components/Card'
 import Footer from '../../components/Footer'
-import { useDispatch, useSelector } from 'react-redux'
+
 import { fetchCars } from '../../actions/car'
+
+import { Navigate } from 'react-router-dom'
 
 export default function Result() {
   const dispatch = useDispatch()
   const dataCar = useSelector((state) => state.carsReducer.cars)
+  const { isLoggedIn } = useSelector(state => state.auth)
 
   const filterCar = dataCar.filter(
     (data) => data.image && data.name && data.price !== null && data.category !== ''
@@ -17,6 +23,11 @@ export default function Result() {
   useEffect(() => {
     dispatch(fetchCars())
   }, [])
+
+
+  if (!isLoggedIn) {
+    return <Navigate to='/unauthorize' />
+  }
 
   return (
     <Fragment>
