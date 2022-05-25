1438099a0f282dab65508a4073104b31da46ad2c
didapatria <didapatria3@gmail.com>
2022-05-26 03:11:39 +0700

    Update index.js

--- challenge-07/src/reducers/index.js
+++ challenge-07/src/reducers/index.js
@@ -2,8 +2,8 @@ import { combineReducers } from 'redux'
 import auth from './auth'
 import message from './message'
 import { carsReducer } from './cars'
-import { carReducer }from './car'
-import { ordersReducer }from './order'
+import { carReducer } from './car'
+import { ordersReducer } from './order'
 
 export default combineReducers({
   auth,
