a0f10b6e75144827e12c02783e0e6117e428376b
didapatria <didapatria3@gmail.com>
2022-05-27 14:26:07 +0700

    Update Nav.jsx

--- challenge-07/src/components/payment/Nav.jsx
+++ challenge-07/src/components/payment/Nav.jsx
@@ -3,7 +3,7 @@ import { FiArrowLeft, FiCheck } from "react-icons/fi";
 export default function Nav() {
   return (
     <div className='flex justify-center mt-10'>
-      <div className='w-[87.5%] flex justify-between'>
+      <div className='w-[87.5%] md:flex space-y-8 md:space-y-0 justify-between'>
         <div className='flex space-x-4'>
           <FiArrowLeft className='text-2xl' />
           <div>
@@ -11,7 +11,7 @@ export default function Nav() {
             <div>Order ID: xxxxxxxx</div>
           </div>
         </div>
-        <div className='flex items-center space-x-2 text-xs'>
+        <div className='flex items-center justify-end space-x-2 text-xs'>
           <FiCheck className='bg-primary rounded-full p-1 w-4 h-4 stroke-white' />
           <div>Pilih Metode</div>
           <div className='bg-primary w-7 h-px'></div>
