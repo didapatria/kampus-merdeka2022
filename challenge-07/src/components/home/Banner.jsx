7f12edb0e69c06ead11fa155c6a172c5a8ca9859
didapatria <didapatria3@gmail.com>
2022-05-27 14:26:03 +0700

    Update Banner.jsx

--- challenge-07/src/components/home/Banner.jsx
+++ challenge-07/src/components/home/Banner.jsx
@@ -8,7 +8,7 @@ export default function Banner() {
         <div className='bg-primary rounded-xl text-white'>
           <div className='py-16'>
             <h1 className='font-bold text-4xl mb-4'>Sewa Mobil di (Lokasimu) Sekarang</h1>
-            <p className='mb-14 w-1/3 mx-auto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            <p className='mb-14 w-4/5 md:w-1/3 mx-auto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. </p>
             <Link to='/search' className='rounded-sm px-5 py-3 text-center font-medium bg-green-500 text-white mt-8' type='submit'>Mulai Sewa Mobil</Link>
           </div>
