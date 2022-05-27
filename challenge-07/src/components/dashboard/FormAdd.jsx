5c48087c46cf5358ba74328867a45bfd8b630a3e
didapatria <didapatria3@gmail.com>
2022-05-27 14:03:54 +0700

    Update FormAdd.jsx

--- challenge-07/src/components/dashboard/FormAdd.jsx
+++ challenge-07/src/components/dashboard/FormAdd.jsx
@@ -17,18 +17,18 @@ export default function FormAdd() {
       <div className='bg-white rounded-sm p-4 space-y-4'>
         <div className='flex items-center space-x-2'>
           <div className='w-1/6'>Nama<span className='text-red-500'>*</span></div>
-          <div className='w-1/3 bg-white border border-gray-500 rounded-sm px-3 py-2'>Placeholder</div>
+          <div className='w-1/3 bg-white border border-gray-500 text-gray-500 rounded-sm px-3 py-2'>Placeholder</div>
         </div>
         <div className='flex items-center space-x-2'>
           <div className='w-1/6'>Harga<span className='text-red-500'>*</span></div>
-          <div className='w-1/3 bg-white border border-gray-500 rounded-sm px-3 py-2'>Placeholder</div>
+          <div className='w-1/3 bg-white border border-gray-500 text-gray-500 rounded-sm px-3 py-2'>Placeholder</div>
         </div>
         <div className='flex items-center space-x-2'>
           <div className='w-1/6'>Foto<span className='text-red-500'>*</span></div>
           <div className='w-1/3 space-y-1'>
-            <div className='bg-white border border-gray-500 rounded-sm px-3 py-2'>
+            <div className='bg-white border border-gray-500 text-gray-500 rounded-sm px-3 py-2'>
               <div className='flex justify-between items-center'>
-                <div>Placeholder</div>
+                <div>IMG/2/2022/001</div>
                 <FiUpload />
               </div>
             </div>
@@ -37,26 +37,26 @@ export default function FormAdd() {
         </div>
         <div className='flex items-center space-x-2'>
           <div className='w-1/6'>Start Rent</div>
-          {/* <div className='w-1/3 bg-white border border-gray-500 rounded-sm px-3 py-2'>Placeholder</div> */}
+          {/* <div className='w-1/3 bg-white border border-gray-500 text-gray-500 rounded-sm px-3 py-2'>Placeholder</div> */}
           <div>-</div>
         </div>
         <div className='flex items-center space-x-2'>
           <div className='w-1/6'>Finish Rent</div>
-          {/* <div className='w-1/3 bg-white border border-gray-500 rounded-sm px-3 py-2'>Placeholder</div> */}
+          {/* <div className='w-1/3 bg-white border border-gray-500 text-gray-500 rounded-sm px-3 py-2'>Placeholder</div> */}
           <div>-</div>
         </div>
         <div className='flex items-center space-x-2'>
           <div className='w-1/6'>Created at</div>
-          {/* <div className='w-1/3 bg-white border border-gray-500 rounded-sm px-3 py-2'>Placeholder</div> */}
+          {/* <div className='w-1/3 bg-white border border-gray-500 text-gray-500 rounded-sm px-3 py-2'>Placeholder</div> */}
           <div>-</div>
         </div>
         <div className='flex items-center space-x-2'>
           <div className='w-1/6'>Updated at</div>
-          {/* <div className='w-1/3 bg-white border border-gray-500 rounded-sm px-3 py-2'>Placeholder</div> */}
+          {/* <div className='w-1/3 bg-white border border-gray-500 text-gray-500 rounded-sm px-3 py-2'>Placeholder</div> */}
           <div>-</div>
         </div>
       </div>
-      <div className='flex space-x-4 mt-6'>
+      <div className='flex space-x-4 mt-72 mb-1'>
         <div className='bg-white border border-primary rounded-sm font-bold text-primary px-3 py-2'>Cancel</div>
         <div className='bg-primary rounded-sm font-bold text-white px-3 py-2'>Save</div>
       </div>
