c36a170c7a9540e085057d7690a401c6ab288b54
didapatria <didapatria3@gmail.com>
2022-05-27 15:27:32 +0700

    add moment and currency formatter

--- challenge-07/src/data/listCar.js
+++ challenge-07/src/data/listCar.js
@@ -1,3 +1,12 @@
+import Moment from 'react-moment'
+
+const currencyFormatter = (value) => {
+  const formattedAmount = Math.trunc(value)
+    .toString()
+    .replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, '$1,')
+  return formattedAmount
+}
+
 const columnsCar = [
   {
     name: 'Id',
@@ -17,27 +26,27 @@ const columnsCar = [
   },
   {
     name: 'Price',
-    selector: row => row.price,
+    selector: row => 'Rp. ' + currencyFormatter(row.price),
     sortable: true,
   },
   {
     name: 'Start Rent',
-    selector: row => row.start_rent_at,
+    selector: row => <Moment format="D MMM YYYY" date={row.start_rent_at} />,
     sortable: true,
   },
   {
     name: 'Finish Rent',
-    selector: row => row.finish_rent_at,
+    selector: row => <Moment format="D MMM YYYY" date={row.finish_rent_at} />,
     sortable: true,
   },
   {
     name: 'Created at',
-    selector: row => row.createdAt,
+    selector: row => <Moment format="D MMM YYYY, hh.mm" date={row.createdAt} />,
     sortable: true,
   },
   {
     name: 'Updated at',
-    selector: row => row.updatedAt,
+    selector: row => <Moment format="D MMM YYYY, hh.mm" date={row.updatedAt} />,
     sortable: true,
   },
 ]
